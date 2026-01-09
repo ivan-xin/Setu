46435b55ed8fa70273ff98c479a025b7ce40ded8
ivan <ivan.coder.zx@gmail.com>
2026-01-09 18:10:55 +0800

    chore:(MPT)update export subnet_state in storage

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -2,6 +2,7 @@ pub mod state;
 pub mod event_store;
 pub mod anchor_store;
 pub mod object_store;
+pub mod subnet_state;
 
 // RocksDB storage implementation
 pub mod rocks;
@@ -12,6 +13,7 @@ pub use state::*;
 pub use event_store::*;
 pub use anchor_store::*;
 pub use object_store::*;
+pub use subnet_state::{SubnetStateSMT, GlobalStateManager};
 
 // Re-export RocksDB types
 pub use rocks::{SetuDB, RocksDBConfig, ColumnFamily, StorageError};
